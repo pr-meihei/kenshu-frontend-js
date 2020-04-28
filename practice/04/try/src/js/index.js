260707a4aaa98b728cd37aa615afbf241655b913
pr-meihei <yohei.ema@prtimes.co.jp>
2020-04-28 17:05:50 +0900

    04

---
+++ practice/04/try/src/js/index.js
@@ -0,0 +1,17 @@
+(function () {
+  "use strict";
+
+  const buttonElements = document.getElementsByClassName("button");
+
+  function onClick(elm) {
+    const listElement = elm.nextElementSibling;
+    return () => {
+      listElement.classList.toggle("is-active");
+    };
+  }
+
+  for (let i = 0; i < buttonElements.length; i++) {
+    let button = buttonElements[i];
+    button.addEventListener("click", onClick(button));
+  }
+})();
