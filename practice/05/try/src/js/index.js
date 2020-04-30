e29471e924bb923246c915803a371f2fd6ccda2c
pr-meihei <yohei.ema@prtimes.co.jp>
2020-04-30 10:47:00 +0900

    Update 05

--- practice/05/try/src/js/index.js
+++ practice/05/try/src/js/index.js
@@ -3,10 +3,21 @@
 
   const inputElement = document.getElementById("input");
 
+  function escapeHtml(str) {
+    str = str.replace(/&/g, "&amp;");
+    str = str.replace(/</g, "&lt;");
+    str = str.replace(/>/g, "&gt;");
+    str = str.replace(/"/g, "&quot;");
+    str = str.replace(/'/g, "&#39;");
+    return str;
+  }
+
   function onKeyUp(elm) {
     const textElement = document.getElementById("text");
     return () => {
-      textElement.innerHTML = /yes/.test(elm.value) ? elm.value : "";
+      textElement.innerHTML = /yes/.test(elm.value)
+        ? escapeHtml(elm.value)
+        : "";
     };
   }
 
